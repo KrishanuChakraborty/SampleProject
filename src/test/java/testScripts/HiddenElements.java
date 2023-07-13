4578d88304b6b55c6625a3bc5a231d3c44fd6f82
Administrator <Administrator@172.31.22.170>
2023-07-13 06:09:39 +0000

    pushed on 13th July 2023

---
+++ src/test/java/testScripts/HiddenElements.java
@@ -0,0 +1,29 @@
+package testScripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HiddenElements {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://automationbookstore.dev/");
+		
+//		WebElement closeIcon = driver.findElement(By.linkText("Clear Text"));
+		WebElement closeIcon =driver.findElement(By.xpath("//a[@title='Clear text']"));
+		System.out.println("Close icon...."+closeIcon.isDisplayed());
+		
+		
+		driver.findElement(By.cssSelector("#searchBar")).sendKeys("Test");
+		Thread.sleep(2000);
+		closeIcon =driver.findElement(By.xpath("//a[@title='Clear text']"));
+		if (closeIcon.isDisplayed()) {
+			closeIcon.click();
+		}
+		System.out.println("Close icon after text.."+closeIcon.isDisplayed());
+	}
+
+}
--- src/test/java/testScripts/LoginPageTest.java
+++ src/test/java/testScripts/LoginPageTest.java
@@ -1,18 +1,33 @@
 package testScripts;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginPageTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://google.com");
 		//driver.findElement(By.xpath("//id="APjFqb"))
 		//  "//input[@id='name']//following::input[2]" // following
 		// "//input[@id='name']//preceding::input[2]" // preceding
+		WebElement schBox = driver.findElement(By.name("q"));
+		schBox.sendKeys("Java tutorial");
+		Thread.sleep(2000);
+		List<WebElement> items = driver.findElements(By.xpath(
+				"(//ul[@class='G43f7e'])[1]//li//descendant::div[@class='wM6W7d']"));
+		System.out.print("No of items =" +items.size());
+		for (int i=0;i<items.size();i++) {
+			System.out.println(items.get(i).getText());
+			if (items.get(i).getText().equalsIgnoreCase("Java tutorial pdf")) {
+				break;
+			}
+		}
 	}
 
 }
