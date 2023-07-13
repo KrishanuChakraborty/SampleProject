0170ab9902b8e886909553333d887f6e4c6e2e5b
Administrator <Administrator@172.31.22.170>
2023-07-13 07:26:47 +0000

    3rd Commit 7th July 2023

--- src/test/java/testScripts/HandlingDropDown.java
+++ src/test/java/testScripts/HandlingDropDown.java
@@ -1,7 +1,10 @@
 package testScripts;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
@@ -15,6 +18,18 @@ public class HandlingDropDown {
 		
 		Select val= new Select(driver.findElement(By.id("select-demo")));
 		val.selectByValue("Wednesday");
+		
+		Select mulSel = new Select(driver.findElement(By.id("multi-select")));
+		mulSel.selectByIndex(1);
+		mulSel.selectByValue("New Jersey");
+		mulSel.selectByVisibleText("New York");
+		
+		List<WebElement> AllSelectedElement =mulSel.getAllSelectedOptions();
+		for (int i=0;i<AllSelectedElement.size();i++) {
+			System.out.println(AllSelectedElement.get(i).getText());
+		}
+		mulSel.deselectByIndex(1);
+		
 	}
 
 }
